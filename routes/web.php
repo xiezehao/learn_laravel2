dce038c5c0fb08404d9f82db7b0ef75220994a78
xiezehao <798351803@qq.com>
2020-06-23 23:40:51 +0800

    1

--- routes/web.php
+++ routes/web.php
@@ -32,8 +32,5 @@ Route::group(['middleware'=>['auth','verified']],function(){
     Route::get('user_address','UserAddressesController@index')->name('user_addresses.index');
     Route::get('user_addresses/create','UserAddressesController@create')->name('user_addresses.create');
     Route::post('user_addresses','UserAddressesController@store')->name('user_addresses.store');
-    Route::post('user_addresses','UserAddressesController@store')->name('user_addresses.store');
-    Route::post('user_addresses','UserAddressesController@store')->name('user_addresses.store');
-    Route::post('user_addresses','UserAddressesController@store')->name('user_addresses.store');
-    Route::post('user_addresses','UserAddressesController@store')->name('user_addresses.store');
+
 });
