3d7a84b3703fde52bb32471e19b4db1b9640dcfe
xiezehao <798351803@qq.com>
2020-06-28 20:17:38 +0800

    商品模型

--- public/index.php
+++ public/index.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Laravel - A PHP Framework For Web Artisans
  *
@@ -51,10 +50,10 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
+
 $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
 );
 
 $response->send();
-
 $kernel->terminate($request, $response);
